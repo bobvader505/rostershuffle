b1007c36b976c4702b61c7630df024930206a1a5
Michael Sherman <bobvader505@gmail.com>
2016-12-11 23:38:20 -0500

    something about a key

--- db/migrate/20161211221621_remove_team_from_players.rb
+++ db/migrate/20161211221621_remove_team_from_players.rb
@@ -1,5 +1,5 @@
 class RemoveTeamFromPlayers < ActiveRecord::Migration[5.0]
   def change
-    remove_reference :players, :team, foreign_key: true
+    remove_reference :players, :team, foreign_key: false
   end
 end
